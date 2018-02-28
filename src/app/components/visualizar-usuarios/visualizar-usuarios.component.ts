8ed6de21ed1698b4997818c5c08ce21486ec3f40
Berenisse Rios <berenisser@gmail.com>
2018-02-28 15:11:38 -0300

    Ficha de Crear Usuario: HTML y CSS listos

--- src/app/components/visualizar-usuarios/visualizar-usuarios.component.ts
+++ src/app/components/visualizar-usuarios/visualizar-usuarios.component.ts
@@ -7,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizarUsuariosComponent implements OnInit {
   nuevoUsuario:string;
+  nuevoUsuarioRun:string;
+  nuevoUsuarioNombre:string;
+  nuevoUsuarioApellidos:string;
+  nuevoUsuarioCorreo:any;
+  nuevoUsuarioTelefono1:number;
+  nuevoUsuarioTelefono2:number;
+  nuevoUsuarioEntidad:string;
+  nuevoUsuarioRol:string;
+  nuevoUsuarioContrasena1:string;
+  nuevoUsuarioContrasena2:string;
 
   constructor() { }
 
   ngOnInit() {
   }
   guargarUsuario(){
-    console.log(this.nuevoUsuario);
+    console.log(this.nuevoUsuario + this.nuevoUsuarioRun + this.nuevoUsuarioNombre + this.nuevoUsuarioApellidos +
+                this.nuevoUsuarioCorreo + this.nuevoUsuarioTelefono1 + this.nuevoUsuarioTelefono2 + this.nuevoUsuarioEntidad +
+                this.nuevoUsuarioRol + this.nuevoUsuarioContrasena1 + this.nuevoUsuarioContrasena2);
   }
 }
