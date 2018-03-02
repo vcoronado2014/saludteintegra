2aa2532ddf49eb1c63cde5a92eee6997f1eba039
vcoronado <vcoronado@saydex.cl>
2018-03-02 19:17:37 -0300

    appps

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,5 +1,5 @@
 export class appSettings{
     //public static API_ENDPOINT = 'http://172.16.0.241/SaludTeIntegra/api/';
-    public static API_ENDPOINT = 'https://catest.saludsidra.cl/saludteintegra/api/';
-    //public static API_ENDPOINT = 'http://localhost:58176/Api/';
+    //public static API_ENDPOINT = 'https://catest.saludsidra.cl/saludteintegra/api/';
+    public static API_ENDPOINT = 'http://localhost:58176/Api/';
 }
